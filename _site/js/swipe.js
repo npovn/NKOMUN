f4d28858d96a1354b079ffa7ab92cc6cde14bcfc
poigiatre <85273780+poigiatre@users.noreply.github.com>
2022-06-09 12:25:05 +0700

    Add files via upload

---
+++ _site/js/swipe.js
@@ -0,0 +1,57 @@
+var currentVal = 0;
+var numberOfPics = 4;
+var autoSwipe = setInterval(function () {
+    turnRight();
+}, 8000);
+var turnOnAutoSwipe = null;
+function controlAutoSwipe() {
+    try {
+        console.log("auto swipe off");
+        clearInterval(autoSwipe);
+        clearTimeout(turnOnAutoSwipe);
+    } catch (error) { }
+    turnOnAutoSwipe = setTimeout(function () {
+        console.log("auto swipe on");
+        autoSwipe = setInterval(function () {
+            turnRight();
+        }, 8000);
+    }, 4000);
+}
+function leftClick() {
+    turnLeft();
+    controlAutoSwipe();
+}
+function rightClick() {
+    turnRight();
+    controlAutoSwipe();
+}
+function turnLeft() {
+    var panel = document.getElementById("panel");
+    var dotSelected = document.getElementById("dot" + currentVal.toString());
+    dotSelected.className = "dot";
+    currentVal -= 1;
+    if (currentVal < 0) currentVal += numberOfPics;
+    panel.style.transform = "translateX(-" + currentVal.toString() + "00%)";
+    var dotWillSelected = document.getElementById("dot" + currentVal.toString());
+    dotWillSelected.className = "dot-selected";
+}
+function turnRight() {
+    var panel = document.getElementById("panel");
+    var dotSelected = document.getElementById("dot" + currentVal.toString());
+    dotSelected.className = "dot";
+    currentVal += 1;
+    if (currentVal >= numberOfPics) currentVal = 0;
+    panel.style.transform = "translateX(-" + currentVal.toString() + "00%)";
+    var dotWillSelected = document.getElementById("dot" + currentVal.toString());
+    dotWillSelected.className = "dot-selected";
+}
+function gotoPage(page) {
+    controlAutoSwipe();
+    var panel = document.getElementById("panel");
+    var dotSelected = document.getElementById("dot" + currentVal.toString());
+    dotSelected.className = "dot";
+    currentVal = page;
+    panel.style.transform = "translateX(-" + currentVal.toString() + "00%)";
+    var dotWillSelected = document.getElementById("dot" + currentVal.toString());
+    dotWillSelected.className = "dot-selected";
+}
