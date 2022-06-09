226ff6a2939701937a99237461a0f5c1cea1286e
poigiatre <85273780+poigiatre@users.noreply.github.com>
2022-06-09 11:31:30 +0700

    Delete js directory

--- js/main.js
+++
@@ -1,5 +0,0 @@
-window.addEventListener("scroll", function(){
-    const Parallax = document.querySelector(".parallax");
-    let offsetY = this.window.scrollY;
-    Parallax.style.transform = "translateY(" + offsetY + "px)";
-})
