9a5fda0963648bd25ce4610121e3d27dafe21455
poigiatre <85273780+poigiatre@users.noreply.github.com>
2022-06-27 23:18:54 +0700

    Add files via upload

--- js/lazyload.js
+++ js/lazyload.js
@@ -3,6 +3,8 @@ function lazyload(id) {
         return;
     }
     var height = (window.innerHeight > 0) ? window.innerHeight : screen.height;
+    console.log(id);
+    console.log($("#" + id).offset().top);
     if ($(window).scrollTop() + height >= $("#" + id).offset().top) {
         $("#" + id).attr('loaded', true);
         var ele = document.getElementById(id);
