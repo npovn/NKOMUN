cdfa1cf3c78b591fee0cce4ade0091a70ba457b0
poigiatre <85273780+poigiatre@users.noreply.github.com>
2022-06-30 10:13:26 +0700

    Add files via upload

--- js/gallery.js
+++ js/gallery.js
@@ -1,57 +1,95 @@
-function infoGallery1(folder, name, description) {
-    document.getElementById("gallery").innerHTML +=
+var data = [
+    {
+        name: 
+        {
+            'vi': 'MOCK MUN #1 21-05-2022',
+            'en': 'MOCK MUN #1 21-05-2022'
+        },
+        description: 
+        {
+            'vi': 'NKO MUN không chỉ có tình yêu - NKO MUN còn nhiều điều hơn thế<br>Vào thứ 7 (21/05/2022) vừa qua, NKO MUN đã có một buổi Training và Showcase Mock MUN diễn ra tại phòng A302.',
+            'en': 'NKO MUN is not only about love - NKO MUN is much more than that<br>Last Saturday (May 21, 2022), NKO MUN had a Training session and Showcase Mock MUN, which took place at room A302.'},
+        folder: './gallery-img/MOCKMUN1-21-05-2022/Ảnh riêng/',
+        img: [
+            {dest: 'Chủ tọa_Chairs.JPG', title: {'vi': 'Chủ toạ', 'en': 'Chairs'}},
+            {dest: 'Đại biểu_Delegates.JPG', title: {'vi': 'Đại biểu', 'en': 'Delegates'}},
+            {dest: 'Quan sát viên_Observers.JPG', title: {'vi': 'Quan sát viên', 'en': 'Observers'}}
+        ],
+    },
+    {
+        name:
+        {
+            'vi': 'TALKSHOW: RETREAT OF THE TIGER',
+            'en': 'TALKSHOW: RETREAT OF THE TIGER'
+        },
+        description:
+        {
+            'vi': 'Lấy bối cảnh vào khoảng thế kỉ 19, khi chủ nghĩa thực dân phát triển và gây ra các hệ quả ảnh hưởng đến nhiều mặt của cuộc sống và con người; khi mà các nền kinh tế phụ thuộc phát triển mạnh, các nền kinh tế bị chuyên môn hóa và thay đổi hoàn toàn nhiều thứ trong xã hội cũ, trong đó có hệ sinh thái. Đồng thời, đây cũng là một trong những lý do chính yếu dẫn đến Chiến tranh Thế giới thứ nhất.  Talkshow "Retreat of The tiger" (tạm dịch: sự trở lại của loài hổ) ra đời nhằm giới thiệu mô hình mô phỏng Liên Hợp Quốc đến cho học sinh của trường Phổ Thông Năng Khiếu và học sinh thuộc các trường THPT trong và ngoài thành phố Hồ Chí Minh, đi kèm với đó là tạo cơ hội cho các bạn có sở thích với Mô phỏng Liên Hợp Quốc và các phạm trù liên quan như Lịch Sử, Kinh Tế,… cùng chia sẻ và bàn luận.',
+            'en': 'This talk show is based on the context of the 19th century when the development of colonialism has resulted in several repercussions to several aspects of our lives, when the interdependence of the economies was prevalent, and also when the economies were specialized and resulted in dramatic changes to the old societies including the ecosystems. By the same token, this explains why World War II occurred.  The talk show “Retreat of The Tiger”(simply translated as the comeback of tigers) wanted to introduce Model United Nations to students of High School For The Gifted-Viet Nam National University as well as that of other high schools.Also, this talk show is  held  in an effort to create opportunities for those interested in Model United Nations and other related socioeconomic problems to discuss and debate on these topics.'
+        },
+        folder: './gallery-img/TALKSHOW_RETREAT-OF-THE-TIGER/Ảnh riêng/',
+        img: [
+            {dest: '1.jpg', title: {'vi': '&nbsp;', 'en': '&nbsp;'}},
+            {dest: '2.jpg', title: {'vi': '&nbsp;', 'en': '&nbsp;'}}
+        ]
+    },
+    {
+        name:
+        {
+            'vi': 'TALKSHOW: VAI TRÒ CỦA CÁ NHÂN TRONG LỊCH SỬ',
+            'en': 'TALKSHOW: ROLE OF INDIVIDUALS IN HISTORY'
+        },
+        description:
+        {
+            'vi': 'Liệu bạn đã từng thắc mắc...<br>Khái niệm cá nhân, vĩ nhân?<br>Vĩ nhân làm nên lịch sử? Hay lịch sử tạo nên vĩ nhân?<br>Quần chúng với lịch sử? Cá nhân trong quần chúng<br>Dù câu trả lời là gì đi nữa, thì sự kiện lần này luôn dành cho bạn đó! Nhằm làm rõ vai trò của cá nhân trong lịch sử thông qua những trường hợp cụ thể ở từng giai đoạn — cổ đại, trung đại, cận đại cũng như biết được khi nào cá nhân trở thành vĩ nhân, Câu lạc bộ Mô phỏng Liên Hợp Quốc trường Phổ Thông Năng Khiếu vô cùng vinh dự được đồng hành cùng các thầy cô tổ bộ môn Lịch sử trường Phổ Thông Năng Khiếu tổ chức chuyên đề "VAI TRÒ CỦA CÁ NHÂN TRONG LỊCH SỬ".',
+            'en': 'Have you ever wondered...<br>What constitutes a great person?<br>Does great people make history? Or does history create great people?<br>The masses with history? Individuals among the masses ?<br>Whatever the answer is, this event is for you! In order to clarify the role of  individuals in history through specific cases in each period — ancient, medieval, and modern ones as well as to know when an individual can become a great man, NKOMUN is extremely delighted to accompany our teachers from the History Department of the High School  For The Gifted to organize the talk show "ROLE OF INDIVIDUALS IN HISTORY".'
+        },
+        folder: './gallery-img/CHUYÊN-ĐỀ _VAI-TRÒ-CỦA-CÁ-NHÂN-TRONG-LỊCH-SỬ/',
+        img: [
+        ]
+    },
+    {
+        name:
+        {
+            'vi': 'RENMUN',
+            'en': 'RENMUN'
+        },
+        description:
+        {
+            'vi': 'Ngày 17, 18 và 19/6 vừa qua, các học sinh từ Câu lạc bộ Mô phỏng Liên Hợp Quốc trường Phổ Thông Năng Khiếu (Năng Khiếu Open Model United Nations) đã tham dự Hội nghị Liên Hợp Quốc Mô phỏng tại trường Quốc tế Renaissance (Quận 7). <br>Cùng tham gia sự kiện lần này, có đoàn học sinh từ trường Quốc tế Việt Úc (VAS), trường Quốc tế Hàn Quốc (KIS) và trường Song ngữ Quốc tế (EMASI). <br>Các học sinh được “vào vai” thành đại biểu, đại diện cho mỗi đất nước, được chia thành 4 hội đồng, cùng thảo luận và tranh luận các vấn đề như:<br>   + Hội đồng Bảo an (Security Council – SC): Tình hình của Mali; Tình hình của Cộng hòa dân chủ Congo; …<br>   + Hội đồng Nhân quyền (Human Rights Council – HRC): Tình hình ở Cộng hòa Dân chủ Congo HRC; Vấn đề giáo dục của các dân tộc thiểu số ở Đông Nam Á, Tự do ngôn luận trong kỷ nguyên kỹ thuật số; …<br>   + Hội đồng Giả tưởng Lịch sử (Historical Council – HC): Tình hình ở Bắc Ireland (1967-1998); Phản ứng của Quốc tế đối với vụ đánh bom ngày 11/9; …<br>   + Đại Hội đồng 1 (General Assembly – GA1): Các loại thuốc gây nghiện bị Thế giới cấm; hay đổi về nhân khẩu học trong thế kỷ 21; Đảm bảo hòa bình trong vũ trụ<br>Sự kiện lần này mang lại cho các bạn học sinh những trải nghiệm mới mẻ, có cơ hội được thể hiện quan điểm cá nhân của mình bằng tiếng Anh với các bạn đồng trang lứa trong suốt 03 ngày làm việc, cùng giao lưu, kết bạn với các trường Quốc tế.<br>Dưới đây là một số hình ảnh trong 03 ngày làm việc của các bạn đại biểu.',
+            'en': ''
+        },
+        folder: './gallery-img/RENMUN/',
+        img: [
+        ]
+    }
+];
+function infoGallery(index, lang) {
+    var a = 
         `<div id="gallery-pop" class="gallery-pop">
         <div class="gallery-popback" onclick=closeGallery()></div>
         <div class="info-box" id="info-box">
-            <h2>${name}</h2>
-            <p>${description}</p>
-            <div class="table">
-                <div class="img-title">
-                    <img src="${folder + 'Chủ tọa_Chairs.JPG'}" alt="">
-                    <h2>Chairs</h2>
-                </div>
-                <div class="img-title">
-                    <img src="${folder + 'Đại biểu_Delegates.JPG'}" alt="">
-                    <h2>Delegates</h2>
-                </div>
-                <div class="img-title">
-                    <img src="${folder + 'Quan sát viên_Observers.JPG'}" alt="">
-                    <h2>Observers</h2>
-                </div>
-            </div>
+            <h2>${data[index].name[lang]}</h2>
+            <p>${data[index].description[lang]}</p>
+            <div class="table">`
+    var fol = data[index].folder;
+    data[index].img.forEach(image => {
+        a+=
+        `<div class="img-title">
+            <img src="${fol + image.dest}" alt="">
+            <h2>${image.title[lang]}</h2>
+        </div>`;        
+    });
+    a +=
+            `</div>
         <div>
     </div>`;
+    document.getElementById("gallery").innerHTML += a;
     setTimeout(() => {
         document.getElementById("info-box").style.opacity = "1";
     }, 1);
 }
-function infoGallery2(folder, name, description) {
-    document.getElementById("gallery").innerHTML +=
-        `<div id="gallery-pop" class="gallery-pop">
-        <div class="gallery-popback" onclick=closeGallery()></div>
-        <div class="info-box" id="info-box">
-            <h2>${name}</h2>
-            <p>${description}</p>
-            <div class="table">
-                <div class="img-title">
-                    <img src="${folder + 'Chủ tọa_Chairs.JPG'}" alt="">
-                    <h2>Chủ tọa</h2>
-                </div>
-                <div class="img-title">
-                    <img src="${folder + 'Đại biểu_Delegates.JPG'}" alt="">
-                    <h2>Đại biểu</h2>
-                </div>
-                <div class="img-title">
-                    <img src="${folder + 'Quan sát viên_Observers.JPG'}" alt="">
-                    <h2>Quan sát viên</h2>
-                </div>
-            </div>
-        <div>
-    </div>`;
-    setTimeout(() => {
-        document.getElementById("info-box").style.opacity = "1";
-    }, 1);
-}
+
 function closeGallery() {
     document.getElementById("gallery").removeChild(document.getElementById("gallery-pop"));
 }
