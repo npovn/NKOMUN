f10aea6de36104c554686b34a22c902ec88a57e4
poigiatre <85273780+poigiatre@users.noreply.github.com>
2022-07-04 00:09:58 +0700

    Add files via upload

--- js/gallery.js
+++ js/gallery.js
@@ -27,8 +27,10 @@ var data = [
             'vi': 'Liệu bạn đã từng thắc mắc...<br>Khái niệm cá nhân, vĩ nhân?<br>Vĩ nhân làm nên lịch sử? Hay lịch sử tạo nên vĩ nhân?<br>Quần chúng với lịch sử? Cá nhân trong quần chúng<br>Dù câu trả lời là gì đi nữa, thì sự kiện lần này luôn dành cho bạn đó! Nhằm làm rõ vai trò của cá nhân trong lịch sử thông qua những trường hợp cụ thể ở từng giai đoạn — cổ đại, trung đại, cận đại cũng như biết được khi nào cá nhân trở thành vĩ nhân, Câu lạc bộ Mô phỏng Liên Hợp Quốc trường Phổ Thông Năng Khiếu vô cùng vinh dự được đồng hành cùng các thầy cô tổ bộ môn Lịch sử trường Phổ Thông Năng Khiếu tổ chức chuyên đề "VAI TRÒ CỦA CÁ NHÂN TRONG LỊCH SỬ".',
             'en': 'Have you ever wondered...<br>What constitutes a great person?<br>Does great people make history? Or does history create great people?<br>The masses with history? Individuals among the masses ?<br>Whatever the answer is, this event is for you! In order to clarify the role of  individuals in history through specific cases in each period — ancient, medieval, and modern ones as well as to know when an individual can become a great man, NKOMUN is extremely delighted to accompany our teachers from the History Department of the High School  For The Gifted to organize the talk show "ROLE OF INDIVIDUALS IN HISTORY".'
         },
-        folder: './gallery-img/CHUYÊN-ĐỀ _VAI-TRÒ-CỦA-CÁ-NHÂN-TRONG-LỊCH-SỬ/',
+        folder: './gallery-img/CHUYÊN-ĐỀ _VAI-TRÒ-CỦA-CÁ-NHÂN-TRONG-LỊCH-SỬ/Ảnh riêng/',
         img: [
+            {dest: '1.jpg', title: {'vi': '&nbsp;', 'en': '&nbsp;'}},
+            {dest: '2.jpg', title: {'vi': '&nbsp;', 'en': '&nbsp;'}}
         ]
     },
     {
@@ -56,11 +58,27 @@ var data = [
         },
         description:
         {
-            'vi': 'Ngày 17, 18 và 19/6 vừa qua, các học sinh từ Câu lạc bộ Mô phỏng Liên Hợp Quốc trường Phổ Thông Năng Khiếu (Năng Khiếu Open Model United Nations) đã tham dự Hội nghị Liên Hợp Quốc Mô phỏng tại trường Quốc tế Renaissance (Quận 7). <br>Cùng tham gia sự kiện lần này, có đoàn học sinh từ trường Quốc tế Việt Úc (VAS), trường Quốc tế Hàn Quốc (KIS) và trường Song ngữ Quốc tế (EMASI). <br>Các học sinh được “vào vai” thành đại biểu, đại diện cho mỗi đất nước, được chia thành 4 hội đồng, cùng thảo luận và tranh luận các vấn đề như:<br>   + Hội đồng Bảo an (Security Council – SC): Tình hình của Mali; Tình hình của Cộng hòa dân chủ Congo; …<br>   + Hội đồng Nhân quyền (Human Rights Council – HRC): Tình hình ở Cộng hòa Dân chủ Congo HRC; Vấn đề giáo dục của các dân tộc thiểu số ở Đông Nam Á, Tự do ngôn luận trong kỷ nguyên kỹ thuật số; …<br>   + Hội đồng Giả tưởng Lịch sử (Historical Council – HC): Tình hình ở Bắc Ireland (1967-1998); Phản ứng của Quốc tế đối với vụ đánh bom ngày 11/9; …<br>   + Đại Hội đồng 1 (General Assembly – GA1): Các loại thuốc gây nghiện bị Thế giới cấm; hay đổi về nhân khẩu học trong thế kỷ 21; Đảm bảo hòa bình trong vũ trụ<br>Sự kiện lần này mang lại cho các bạn học sinh những trải nghiệm mới mẻ, có cơ hội được thể hiện quan điểm cá nhân của mình bằng tiếng Anh với các bạn đồng trang lứa trong suốt 03 ngày làm việc, cùng giao lưu, kết bạn với các trường Quốc tế.<br>Dưới đây là một số hình ảnh trong 03 ngày làm việc của các bạn đại biểu.',
-            'en': ''
+            'vi': 'Ngày 17, 18 và 19/6 vừa qua, các học sinh từ Câu lạc bộ Mô phỏng Liên Hợp Quốc trường Phổ Thông Năng Khiếu (Năng Khiếu Open Model United Nations) đã tham dự Hội nghị Liên Hợp Quốc Mô phỏng tại trường Quốc tế Renaissance (Quận 7). <br>Các học sinh được “vào vai” thành đại biểu, đại diện cho mỗi đất nước, được chia thành 4 hội đồng, cùng thảo luận và tranh luận các vấn đề như:<br>&nbsp;&nbsp;&nbsp;+ Hội đồng Bảo an (Security Council – SC)<br>&nbsp;&nbsp;&nbsp;+ Hội đồng Nhân quyền (Human Rights Council – HRC)<br>&nbsp;&nbsp;&nbsp;+ Hội đồng Giả tưởng Lịch sử (Historical Council – HC)<br>&nbsp;&nbsp;&nbsp;+ Đại Hội đồng 1 (General Assembly – GA1)<br>Dưới đây là một số hình ảnh trong 03 ngày làm việc của các bạn đại biểu.<br>',
+            'en': 'From the last 17th to the 19th of June, Nang Khieu Open Model United Nations students have attended the Open Model United Nations conference at Renaissance International School (District 7).<br>They joined as delegates, representing different countries, divided into four committees to cite and discuss such issues: <br>&nbsp;&nbsp;&nbsp;+ Security Council – SC<br>&nbsp;&nbsp;&nbsp;+ Human Rights Council – HRC<br>&nbsp;&nbsp;&nbsp;+ Historical Council – HC<br>&nbsp;&nbsp;&nbsp;+ General Assembly – GA1<br>The pictures below were taken from the delegates’ three days of working.'
         },
-        folder: './gallery-img/RENMUN/',
+        folder: './gallery-img/RENMUN/Ảnh riêng/',
         img: [
+            {dest: '1.jpg', title: {'vi': '&nbsp;', 'en': '&nbsp;'}},
+            {dest: '2.jpg', title: {'vi': '&nbsp;', 'en': '&nbsp;'}},
+            {dest: '3.jpg', title: {'vi': '&nbsp;', 'en': '&nbsp;'}},
+            {dest: '4.jpg', title: {'vi': '&nbsp;', 'en': '&nbsp;'}},
+            {dest: '5.jpg', title: {'vi': '&nbsp;', 'en': '&nbsp;'}},
+            {dest: '6.jpg', title: {'vi': '&nbsp;', 'en': '&nbsp;'}},
+            {dest: '7.jpg', title: {'vi': '&nbsp;', 'en': '&nbsp;'}},
+            {dest: '8.jpg', title: {'vi': '&nbsp;', 'en': '&nbsp;'}},
+            {dest: '9.jpg', title: {'vi': '&nbsp;', 'en': '&nbsp;'}},
+            {dest: '10.jpg', title: {'vi': '&nbsp;', 'en': '&nbsp;'}},
+            {dest: '11.jpg', title: {'vi': '&nbsp;', 'en': '&nbsp;'}},
+            {dest: '12.jpg', title: {'vi': '&nbsp;', 'en': '&nbsp;'}},
+            {dest: '13.jpg', title: {'vi': '&nbsp;', 'en': '&nbsp;'}},
+            {dest: '14.jpg', title: {'vi': '&nbsp;', 'en': '&nbsp;'}},
+            {dest: '15.jpg', title: {'vi': '&nbsp;', 'en': '&nbsp;'}},
+            {dest: '16.jpg', title: {'vi': '&nbsp;', 'en': '&nbsp;'}},
         ]
     }
 ];
@@ -71,7 +89,7 @@ function infoGallery(index, lang) {
         <div class="info-box" id="info-box">
             <h2>${data[index].name[lang]}</h2>
             <p>${data[index].description[lang]}</p>
-            <div class="table">`
+            <div class="table" style="grid-template-columns: repeat(${Math.min(data[index].img.length,3)}, 1fr);">`
     var fol = data[index].folder;
     data[index].img.forEach(image => {
         a+=
